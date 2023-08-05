b1ca3aa638d7233c27d32d3d1ddc159b828ee67c
ShmidtAlex <shmidt.alex@gmail.com>
2023-08-05 16:41:37 +0200

    for ai

--- client/modules/AuthorizationForm/api/auth.ts
+++ client/modules/AuthorizationForm/api/auth.ts
@@ -15,18 +15,16 @@ export default class AuthorizationService extends BaseHttpService<IAxiosConfig> 
   
   async registration(email: string, password: string): Promise<AxiosResponse> {
     const { data } = await this.axiosClient.post(`api/user/registration`, {email, password})
-    localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
   }
   
   async login(email: string, password: string): Promise<AxiosResponse> {
     const { data } = await this.axiosClient.post(`api/user/login`, {email, password})
-    localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
   }
-  async check(): Promise<AxiosResponse> {
-    const { data } = this.axiosClient.get(`api/user/auth`)
-    localStorage.setItem('token', data.token)
+  async check(user): Promise<AxiosResponse> {
+    console.log('user',user)
+    const { data } = this.axiosClient.get(`api/user/auth`, user)
     return jwt_decode(data.token)
   }
 }
