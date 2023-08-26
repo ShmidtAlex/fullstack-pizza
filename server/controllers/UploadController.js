b394dc31996d7dec4750319b4c17622a2f9aec9c
ShmidtAlex <shmidt.alex@gmail.com>
2023-08-26 14:07:25 +0200

    multer set properly, but doesn't return anything to frontend

--- server/controllers/UploadController.js
+++ server/controllers/UploadController.js
@@ -1,15 +1,40 @@
 const ApiError = require('../error/ApiError');
+const multer = require('multer')
+const stream = require('stream');
 class UploadsController {
+  filename(_req, file, cb) {
+    console.log('file will be named ' + file.originalname);
+    cb(null, file.originalname);
+  }
   async uploadPhoto(req, res, next) {
-    console.log('CONTROLLER', req.body, req.files)
+    // Todo: move multer into separate middleware (in order to keep controller thin)?
+    const storage = multer.diskStorage({
+      destination: 'uploads/',
+      filename: (_req, file, cb) => {
+        cb(null, file.name);
+      }
+    });
     try {
-      if (!req.file) {
+      if (!req.files.image) {
         return next(ApiError.badRequest('No file uploaded.'));
       }
-
-      const imageUrl = req.file.filename; // Use the generated filename or customize as needed
-
-      return res.json({ imageUrl });
+      storage._handleFile(
+        req, {
+          ...req.files.image,
+          stream: new stream.PassThrough(),
+        },
+        (err) => {
+          if (err) {
+            console.log('Upload failed with error: ' + err.message);
+            res.status(422);
+            res.json(err.message);
+            res.end();
+          }
+        }
+      );
+      res.status(200);
+      res.json('OK');
+      res.end();
     } catch (e) {
       return next(ApiError.internal(e.message));
     }
--- server/routes/uploadRouter.js
+++ server/routes/uploadRouter.js
@@ -1,10 +1,8 @@
-// uploadRouter.js
 const Router = require('express');
 const router = new Router();
 const uploadsController = require("../controllers/UploadController");
 const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
-const multerMiddleware = require("../middleware/multerMiddleware")
 
-router.post('/', checkRoleMiddleware(['SUPERADMIN', 'ADMIN']), multerMiddleware, uploadsController.uploadPhoto);
+router.post('/', checkRoleMiddleware(['SUPERADMIN', 'ADMIN']), uploadsController.uploadPhoto);
 
 module.exports = router;
